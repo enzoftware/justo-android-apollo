d2802cd435e48d9134f8a43c9d0303faa94ae966
Enzo Lizama <lizama.enzo@gmail.com>
2020-06-02 12:55:40 -0500

    :tada: initial commit

---
+++ app/src/main/java/com/enzoftware/androidgraphql/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.enzoftware.androidgraphql
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
